68e9f259620e95c695acb4599669d4674f6df474
1-МД-20 Громов Иона <99740478+Jonashron@users.noreply.github.com>
2022-02-22 17:40:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def first():
+    name = "Jonah"
+    print(name)
+    return 0
+def second():
+    name = "Jonah"
+    age = 14
+    print(name, " hi! Your age is ", age)
+    return 0
+def third():
+    name = " Jonah"
+    name = name * 5
+    print(name)
+    return 0
+def fourth_5():
+    name = input("Insert your name - ")
+    age = input("Insert your age - ")
+    if int(age) > 10:
+        print("Hi,",name,"! Joke > 10")
+    else:
+        print("Hi,",name,"! Joke <= 10")
+    return 0
+def sixth():
+    name = input("Insert your name - ")
+    print(name[-2:0:-1])
+    print(name[-3:])
+    print(name[:5])
+    return 0
+def seventh():
+    name = input("Insert your name - ")
+    print(len(name))
+    sum = 0
+    prod = 1
+    for i in range(0, len(name)):
+        sum = sum + int(name[i])
+        prod = prod * int(name[i])
+    print(sum)
+    print(prod)
+    return 0
+def eightth():
+    name = input("Insert your name - ")
+    print(name.lower())
+    print(name.upper())
+    return 0
+def ninth():
+    ans = input("2+2*2 = ")
+    if int(ans) != 6:
+        print("wrong")
+    else:
+        print("right")
+    return 0
+ninth()
